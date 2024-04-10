0ea3a64e498dea9ca13bb5d056282abff54c992e
Udayraj <for.udayraj@gmail.com>
2024-04-11 00:53:29 +0530

    first file added

---
+++ prac.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    int i = 42;
+    int j = 78;
+    printf("%d\n",i);
+    printf("%d\n",j);
+    printf("Hello World!\n");
+    printf("new line");
+    return 0;
+}
