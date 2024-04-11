28a1a2ae20d0426a133841e84be2f69f15dfbe33
Udayraj <for.udayraj@gmail.com>
2024-04-11 14:13:01 +0530

    new changes

--- prac.c
+++ prac.c
@@ -4,8 +4,12 @@ int main()
 {
     int i = 42;
     int j = 78;
-    printf("%d\n",i);
-    printf("%d\n",j);
+    char ch = 'd';
+    char a = "f";
+    printf("%c", a);
+    printf("%c", ch);
+    printf("%d\n", i);
+    printf("%d\n", j);
     printf("Hello World!\n");
     printf("new line");
     return 0;
