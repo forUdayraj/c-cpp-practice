a84085118fcd34baf9480302ea8bd95d8dc0bd96
Udayraj <for.udayraj@gmail.com>
2024-04-11 15:34:32 +0530

    adding constant values for the age

---
+++ constant.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main(){
+    const int age = 22;
+    float pi = 3.14;
+    char name = 'u';
+    
+    printf("age is %d\n",age);
+    printf("pi value is %f\n",pi);
+    printf("name value is %c\n",name);
+
+}
--- input.c
+++ input.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 
-int main(){
-    int a,b;
+int main()
+{
+    int a, b;
     printf("enter a");
-    scanf("%d",&a);
+    scanf("%d", &a);
     printf("enter b");
-    scanf("%d",&b);
+    scanf("%d", &b);
     // int sum = a+b;
-    printf("sum is %d",a+b);
+    printf("sum is %d", a + b);
     return 0;
 }
